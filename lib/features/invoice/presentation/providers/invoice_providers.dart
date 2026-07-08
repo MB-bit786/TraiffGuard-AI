098664a8905ba49d5302646f96f68e53e6ff07dc
MOHAMMED BOOKWALA <mohammedbookwala786@gmail.com>
2026-07-08 17:03:03 +0530

    08-07-2026 commit

---
+++ lib/features/invoice/presentation/providers/invoice_providers.dart
@@ -0,0 +1,28 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hscode_auditor/features/invoice/domain/repository/invoice_repository.dart';
+import 'package:hscode_auditor/features/invoice/data/repository/invoice_repository_impl.dart';
+import 'package:hscode_auditor/features/invoice/data/data_sources/invoice_local_data_source.dart';
+import 'package:hscode_auditor/features/invoice/data/data_sources/invoice_remote_data_source.dart';
+import 'package:hscode_auditor/core/util/sql_database_service.dart';
+import 'package:hscode_auditor/features/invoice/domain/usecases/invoice_use_cases.dart';
+import 'package:hscode_auditor/core/util/gemini_audit_service.dart';
+import 'package:hscode_auditor/core/util/auth_service.dart';
+
+final invoiceRepositoryProvider = Provider<InvoiceRepository>((ref) {
+  return InvoiceRepositoryImpl(
+    localDataSource: InvoiceLocalDataSourceImpl(ref.watch(sqlDatabaseServiceProvider)),
+    remoteDataSource: InvoiceRemoteDataSourceImpl(),
+    dbService: ref.watch(sqlDatabaseServiceProvider),
+  );
+});
+
+final invoiceUseCasesProvider = Provider<InvoiceUseCases>((ref) {
+  final repository = ref.watch(invoiceRepositoryProvider);
+  final aiService = ref.watch(geminiAuditServiceProvider);
+  final authService = ref.watch(authServiceProvider);
+  return InvoiceUseCases(
+    repository: repository,
+    aiService: aiService,
+    authService: authService,
+  );
+});
